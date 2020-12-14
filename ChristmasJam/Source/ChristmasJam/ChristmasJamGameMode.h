feed55fd2f4e965739aae51a6157b94f8ff4ad80
Cedric CHAUVIN <cedric.chauvin@edu.devinci.fr>
2020-12-14 09:47:51 +0100

    Project

---
+++ ChristmasJam/Source/ChristmasJam/ChristmasJamGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChristmasJamGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AChristmasJamGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AChristmasJamGameMode();
+};
+
+
+
